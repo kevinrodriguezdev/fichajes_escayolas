fd4be1a7b41caf108056233eaaec69affb05983e
Kevin Rodriguez <kevinrodriguezdev@gmail.com>
2025-03-18 15:09:48 +0100

    first commit

---
+++ resources/js/pages/users/Index.vue
@@ -0,0 +1,12 @@
+<script setup lang ="ts">
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Users" />
+    <AppLayout>
+        <h1>Users</h1>
+    </AppLayout>
+        
+    </template>
